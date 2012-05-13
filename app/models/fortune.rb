af5145d82a763d643373550f402653e7ca55f0ad
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-13 10:28:12 +0200

    dodanie wyszkiwania

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -1,3 +1,7 @@
 class Fortune < ActiveRecord::Base
   attr_accessible :cytat, :zrodlo
+  def self.search(search, page)
+  paginate :per_page => 4, :page => page,
+           :conditions => ['cytat like ?', "%#{search}%"],:order => 'cytat'
+end
 end
